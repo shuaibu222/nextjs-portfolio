0bc300469b3571ac5bdeec149283123beb0721bc
shuaibu <shuaibuabdulkadir222@gmail.com>
2023-05-31 14:03:59 +0100

    configuring evironmental variables

--- sanity.js
+++ sanity.js
@@ -2,11 +2,11 @@ import { createClient } from '@sanity/client';
 import imageUrlBuilder from '@sanity/image-url';
 
 export const client = createClient({
-  projectId: process.env.PROJECT_ID_KEY,
+  projectId: process.env.NEXT_PUBLIC_PROJECT_ID_KEY,
   dataset: 'production',
   useCdn: true,
   apiVersion: '2023-05-29',
-  token: process.env.TOKEN_KEY,
+  token: process.env.NEXT_PUBLIC_TOKEN_KEY,
 });
 
 const builder = imageUrlBuilder(client);
