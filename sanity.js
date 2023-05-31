61a5361e925d0515cad6e1d8531e50f208a12656
shuaibu <shuaibuabdulkadir222@gmail.com>
2023-05-31 12:57:41 +0100

    adding dotenv package and resolving it

--- sanity.js
+++ sanity.js
@@ -2,12 +2,11 @@ import { createClient } from '@sanity/client';
 import imageUrlBuilder from '@sanity/image-url';
 
 export const client = createClient({
-  projectId: 'o250rvq1',
+  projectId: process.env.PROJECT_ID_KEY,
   dataset: 'production',
   useCdn: true,
   apiVersion: '2023-05-29',
-  token:
-    'skeSiOYCnzO7seZMluQi5Hb3jldYxWjd8L9od6p5HVM0W2h62ycZy6IS1ye7otiBdPLVdOesNqyEPxQw6XOVUsksnUSl2ZqYgCLceuLx5IWpcfTK4WW0DsdPqDJoKASby8zeK50H7fLqKVxR02UQYKDM6NXUZmVOhkFAmVPFfBPXk80m8ZOC',
+  token: process.env.TOKEN_KEY,
 });
 
 const builder = imageUrlBuilder(client);
