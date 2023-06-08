68e6063355148f243595a7f4c3ad2d2554272af0
shuaibu <shuaibuabdulkadir222@gmail.com>
2023-06-08 21:34:17 +0100

    adding favicon

--- app/layout.js
+++ app/layout.js
@@ -1,7 +1,6 @@
 import './globals.css';
 import { Poppins } from 'next/font/google';
 import Navigation from './components/Navigation';
-import Footer from './components/Footer';
 
 const poppins = Poppins({ subsets: ['latin'], weight: '300' });
 
