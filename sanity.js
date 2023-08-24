d2ec4fdc13533bae74a005aaf07c6b54070499e8
shuaibu <shuaibuabdulkadir222@gmail.com>
2023-08-24 11:26:51 +0100

    modification on CSS

--- sanity.js
+++ sanity.js
@@ -2,11 +2,11 @@ import { createClient } from '@sanity/client';
 import imageUrlBuilder from '@sanity/image-url';
 
 export const client = createClient({
-  projectId: process.env.NEXT_PUBLIC_PROJECT_ID_KEY,
+  projectId: 'o250rvq1',
   dataset: 'production',
   useCdn: false,
   apiVersion: '2023-05-29',
-  token: process.env.NEXT_PUBLIC_TOKEN_KEY,
+  token: 'skaKvkYBfLWNNJmvSGCZdi7T09QDu5rTy1Mot8q1eyxsHCLfoMOKQ27Ogi3zjKzdv2IkbJHhdx3rlGvZuiFj3kgqMju9G7ZnxzEbErod5X22rCI1qmRGSwKsKG8O9ntCa3g7eBDRXEn0YCKp9HQZHJHJHxTrPH5hoAZxvgpby4PGwRMLqudv',
 });
 
 const builder = imageUrlBuilder(client);
