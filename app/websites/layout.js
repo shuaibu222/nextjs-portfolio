ff7a13cded3d027d0c6be2f7594c5fc333251da0
shuaibu <shuaibuabdulkadir222@gmail.com>
2023-10-30 17:49:16 +0100

    add backend skills

--- app/websites/layout.js
+++
@@ -1,8 +0,0 @@
-export const metadata = {
-  title: 'Shuaibu Abdulkadir',
-  description: 'My personal portfolio description',
-};
-
-export default function WebsitesLayout({ children }) {
-  return <main>{children}</main>;
-}
